b3b0df1e9eb61fb58c62ba4f66c99fcce7312697
Ekaterina Paramonova <lol.nanny@mail.ru>
2025-04-29 11:59:33 +0300

    created components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import BookList from "./components/BookList/BookLIst";
+import BookForm from "./components/BookForm/BookForm";
+//import Filter from "./components/Filter/Filter";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h1>Book Library App</h1>
       </header>
+      <main className="app-main">
+        <div className="app-left-column">
+          <BookForm />
+        </div>
+        <div className="app-right-column">
+          <BookList />
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./BookForm.css";
+
+function BookForm() {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (title && author) {
+      setTitle("");
+      setAuthor("");
+    }
+  };
+  return (
+    <div className="app-block book-form">
+      <h2>Add a new book</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            value={author}
+            onChange={(e) => {
+              setAuthor(e.target.value);
+            }}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="author">Author:</label>
+          <input
+            type="text"
+            id="author"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default BookForm;
