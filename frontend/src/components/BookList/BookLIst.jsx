0641f820ba708ee32b2038552d957a6379d4f5f4
Ekaterina Paramonova <lol.nanny@mail.ru>
2025-04-30 13:50:18 +0300

    added book deletion

--- frontend/src/components/BookList/BookLIst.jsx
+++ frontend/src/components/BookList/BookLIst.jsx
@@ -1,15 +1,35 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import "./BookList.css";
+import { deleteBook } from "../../redux/books/actionCreators";
 
 function BookList() {
+  const dispatch = useDispatch();
   const books = useSelector((state) => state.books);
+  const handleBookDelete = (bookId) => {
+    dispatch(deleteBook(bookId));
+  };
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
       {!!books ? (
         <ul>
           {books.map((book) => {
-            return <li key={book.id}>{`${book.title} by ${book.author}`}</li>;
+            return (
+              <li key={book.id}>
+                <div className="book-info">
+                  {`${book.title} by ${book.author}`}
+                </div>
+                <div className="book-actions">
+                  <button
+                    onClick={() => {
+                      handleBookDelete(book.id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </li>
+            );
           })}
         </ul>
       ) : (
